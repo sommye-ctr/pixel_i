442775bf678b78bfd82112d9e52341aa869252d7
Somye <anyone.mahajan@gmail.com>
2025-12-04 19:19:04 +0530

    Added null=true for coordinator

--- events/models.py
+++ events/models.py
@@ -1,14 +1,14 @@
 import uuid
 
 from django.db import models
+
 from accounts.models import CustomUser
 from events.permissions import EventPermission
 
 
 class Event(models.Model):
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=120, null=False, blank=False)
-    coordinator = models.ForeignKey(CustomUser, on_delete=models.SET_NULL)
+    coordinator = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
     read_perm = models.CharField(choices=EventPermission, max_length=3, default=EventPermission.PUBLIC)
     write_perm = models.CharField(choices=EventPermission, max_length=3, default=EventPermission.PUBLIC)

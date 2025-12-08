a5ddd49cd25d40950cdbed957d5882126c6497a3
Somye <anyone.mahajan@gmail.com>
2025-12-08 13:30:47 +0530

    Added firebase services

--- photos/apps.py
+++ photos/apps.py
@@ -1,6 +1,13 @@
+import firebase_admin
 from django.apps import AppConfig
+from firebase_admin import credentials
 
 
 class PhotosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'photos'
+
+    def ready(self):
+        if not firebase_admin._apps:
+            cred = credentials.Certificate('pixel-i.json')
+            firebase_admin.initialize_app(cred)

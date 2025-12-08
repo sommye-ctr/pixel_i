2ef169bef3809dbc48d39b0a2bedbdc01d903f6d
Somye <anyone.mahajan@gmail.com>
2025-12-08 13:36:50 +0530

    Added firebase storage bucket credentials

--- photos/apps.py
+++ photos/apps.py
@@ -1,5 +1,8 @@
+import os
+
 import firebase_admin
 from django.apps import AppConfig
+from dotenv import load_dotenv
 from firebase_admin import credentials
 
 
@@ -8,6 +11,9 @@ class PhotosConfig(AppConfig):
     name = 'photos'
 
     def ready(self):
+        load_dotenv()
         if not firebase_admin._apps:
             cred = credentials.Certificate('pixel-i.json')
-            firebase_admin.initialize_app(cred)
+            firebase_admin.initialize_app(cred, {
+                'storageBucket': os.getenv('FIREBASE_BUCKET_LINK')
+            })

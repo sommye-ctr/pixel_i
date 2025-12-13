de243a2f7092cae415d0eac15eab6ad19a6d865b
Somye <anyone.mahajan@gmail.com>
2025-12-13 13:28:26 +0530

    Added notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from packaging.utils import _
+
+from accounts.models import CustomUser
+
+
+class Notification(models.Model):
+    class NotificationVerb(models.Choices):
+        TAGGED = "TAGGED", _
+        LIKED = "LIKED", _
+        COMMENTED = "COMMENTED", _
+        PHOTO_ADDED = "PHOTO_ADDED", _
+
+    class NotificationTarget(models.Choices):
+        PHOTO = "PHOTO", _
+        ALBUM = "ALBUM", _
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    recipient = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False,
+                                  related_name="notifications_received")
+    actor = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, related_name="notifications_sent")
+    verb = models.CharField(max_length=50, choices=NotificationVerb, blank=False, null=False)
+    target_type = models.CharField(max_length=50, choices=NotificationTarget, blank=False, null=False)
+    target_id = models.UUIDField(blank=False, null=False)
+    data = models.JSONField(default=dict, blank=True)
+
+    read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["recipient", "read"]),
+            models.Index(fields=["recipient", "-timestamp"]),
+        ]
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return f"Notification from {self.actor_id} to {self.recipient_id}"
